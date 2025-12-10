0f0065735028f2d0a9899519cb15db404368b6e6
Amx0502 <2845713882@qq.com>
2025-12-10 21:47:40 +0800

    添加23,24页

--- src/views/cat-black.vue
+++ src/views/cat-black.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="four-container">
-    <div class="image-container">
-      <video ref="firstVideo" class="top-image" :src="videoSrc1" autoplay muted loop></video>
+    <div class="image-container" ref="firstVideoContainer">
+      <video ref="firstVideo" class="top-image" :src="videoSrc1" autoplay muted @ended="handleFirstVideoEnded"></video>
+    </div>
+    <div class="video-container" ref="secondVideoContainer">
+      <video ref="sayVideo" class="top-image" :src="videoSrc2" muted loop></video>
     </div>
   </div>
 </template>
@@ -12,23 +15,51 @@ export default {
   data() {
     return {
       videoSrc1: '/image/cat-black.mp4',
+      videoSrc2: '/image/cat-black2.mp4',
     }
   },
   methods: {
-    // 视频已设置loop属性，无需额外处理
+    handleFirstVideoEnded() {
+      // 视频1播放结束，隐藏视频1容器
+      const firstContainer = this.$refs.firstVideoContainer;
+      if (firstContainer) {
+        firstContainer.style.display = 'none';
+      }
+      
+      // 显示视频2容器并开始播放
+      const secondContainer = this.$refs.secondVideoContainer;
+      if (secondContainer) {
+        secondContainer.style.display = 'block';
+      }
+      
+      const secondVideo = this.$refs.sayVideo;
+      if (secondVideo) {
+        secondVideo.play();
+      }
+    },
   }
 }
 </script>
 
 <style scoped>
 .four-container {
-  display: flex;
+  position: relative;
+  height: 100vh;
   background-color: #0d97ed;
+  overflow: hidden;
 }
 
-.image-container {
+.image-container,
+.video-container {
+  position: absolute;
+  top: 0;
+  left: 0;
   width: 100%;
-  display: flex;
+  height: 100%;
+}
+
+.video-container {
+  display: none; /* 初始隐藏视频2 */
 }
 
 .top-image {
